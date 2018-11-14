f6ee22e085712558e7dfd86dfa41494b15595e5a
Feiteng <feiteng.li@nyu.edu>
2018-11-14 00:11:09 -0500

    demo app
    Not allowed to upload more than 100 files directly, thus uploading the main activity fow now

---
+++ MainActivity.java
@@ -0,0 +1,26 @@
+package com.lifeiteng.demoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void clickFunction(View view)
+    {
+        ImageView imgview = (ImageView) findViewById(R.id.imageView);
+        imgview.setImageResource(R.drawable.cat);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
